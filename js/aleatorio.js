4947049944ac5f3172f8cd623069cf9d6b98a18a
bozzero <134062506+bozzero@users.noreply.github.com>
2024-08-29 15:17:12 -0300

    aleatorio.js

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,1 +1,8 @@
+const nomes = ["Pamela Voorhees", " Jason Voorhees", "Richie Amber", "Wes Mindy", "Belzebu", "Pazúzu"];
 
+export function aleatorio (lista){
+    const posicao = Math.floor(Math.random()* lista.length);
+    return lista[posicao];
+}
+
+export const nome = aleatorio(nomes)
