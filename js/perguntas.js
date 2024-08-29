dc62df9607169d3cddf870f7eb75f1b9461d4dd6
bozzero <134062506+bozzero@users.noreply.github.com>
2024-08-29 16:03:23 -0300

    perguntas.js

--- js/perguntas.js
+++ js/perguntas.js
@@ -1,1 +1,107 @@
-
+export const perguntas = [
+    {
+        enunciado: ""É fã de filme de terror? Adora ver os mais medrosos tomando sustos. Eles foram avisados, eles estavam condenados e, na sexta-feira, nada os salvará. Qual o nome do assassino de Sexta-feira 13? (1980)"`,?",
+        alternativas: [
+            {
+                texto: ""Pamela Voorhee",
+                afirmacao: [
+                    "Todos desconfião que seja ela.",
+                    "Você acha que pode ser ela."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: ""Jason Voorhee",
+                afirmacao: [
+                    "Todos acham que ele é inocente.",
+                    "Sera ele o assassino."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: ""Pânico 5. Vinte e cinco anos após uma série de crimes brutais chocar a tranquila Woodsboro, um novo assassino se apropria da máscara de Ghostface e começa a perseguir um grupo de adolescentes para trazer à tona segredos do passado mortal da cidade. Quem são os assassinos em Pânico 5?"
+        alternativas: [
+            {
+                texto: ""Richie e Amber.",
+                afirmacao: [
+                    "sera que essa dupla é inocente.",
+                    "Eles são um casal.",                ],
+               ],
+                proxima: 3,
+            },
+            {
+                texto: "Wes e Mindy "
+                afirmacao: [
+                    "Podem ser os assassinos.",
+                    "Eles não são um casal.",
+                 ],                    
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Teste Terrortástico. Você é fã de filmes de terror? Prove acertando essas perguntas sobre os gêneros. Qual filme de terror se tornou um ícone LGBTQIA+ após a Netflix acidentalmente colocá-lo na categoria de filmes LGBTQIA?",
+        alternativas: [
+            {
+                texto: ""Invocação do Mal.",
+                afirmacao: [
+                    "Um dos filmes mais assistidos.",
+                    "Um dos mais assustadores."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: ""Babadook.",
+                afirmacao: [
+                    "Um filme que fala sobre a morte de seu marido.",
+                    "Um monstro te assombra."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "`"O Exorcista. Uma atriz vai gradativamente tomando consciência de que sua filha está tendo um comportamento assustador. Sua mãe pede ajuda a um padre, e este chega à conclusão de que a garota está possuída pelo demônio. No filme 'O Exorcista' (1973), qual o nome do demônio que possui a delegada Regan?",
+        alternativas: [
+            {
+                texto: "Belzebu.",
+                afirmacao: [
+                    "Um dos mais procurados.",
+                    "Ele pode ser o assassino."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Pazúzu.",
+                afirmacao: [
+                    "Ele esta sendo colocado como suspeito.",
+                    "Porém ele não é o assassino."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "`"Filme de Terror. Desde 1996, Ghostface tem assombrado os telefones alheios e feito a icônica pergunta: 'Qual seu filme de terror favorito?' Porém, o assassino não se mantém apenas nessa curiosidade. Trazendo uma série de perguntas, entre fáceis e difíceis, o personagem sempre consegue surpreender suas vítimas.?",
+        alternativas: [
+            {
+                texto: ""Se prepare para uma ligação inesperada com esse quiz que irá desafiar seus conhecimentos sobre horror.",
+                afirmacao: [
+                    "Espero que você estaja pronto.",
+                    "Cuidado ao atender o celular."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: " "Preparado, não esqueça de cometar o que achou da ligação.",
+                afirmacao: [
+                    "cuidado com seu celular.",
+                    "Espero que nao tenha ficado com medo."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
